c186038fdf2271507b9a1da16ae2ed4551b24b38
Dominic Chen <cchendominic@gmail.com>
2026-07-20 16:57:18 -0400

    Define a struct in C

--- main.c
+++ main.c
@@ -11,13 +11,34 @@ typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
 
+typedef i8 b8;
+typedef i32 b32;
 
 typedef float f32;
 typedef double f64;
 
+typedef struct {
+    f32 x;
+    f32 y;
+} vec2f ;
+
 int main() {
+    vec2f v = {
+        .y = 1,
+    };
+
+    printf("Vector = < %f, %f >\n", v.x, v.y);
+
+    v = (vec2f) {
+        .x = 3, .y = 4
+    };
+
+    printf("Vector = < %f, %f >\n", v.x, v.y);
     
-    printf("Hello World\n");
+    v.y = 12;
+    v.x = 0;
+
+    printf("Vector = < %f, %f >\n", v.x, v.y);
 
     return 0;
 }
